44f7cf049798f06997f105f58f0b3d0bfb3a2759
mashabelsol <mb4035@nyu.edu>
2021-12-03 16:07:59 -0500

    done

---
+++ gatsby-config.js
@@ -0,0 +1,27 @@
+const basePath = '/mith301-project'
+
+module.exports = {
+  pathPrefix: basePath,
+  siteMetadata: {
+    title: `Edition numérique: « Portraits contemporains » de la Comtesse Dash`,
+    description: `Edition numérique collaborative des « Portraits contemporains » de la Comtesse Dash.`,
+    author: `FREN659 'Littérature et Presse au XIXème siècle' at the University of Maryland`
+  },
+  plugins: [
+    `gatsby-plugin-material-ui`,
+    `gatsby-theme-ceteicean`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `src/content/tei`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `src/content/pages`,
+        name: `html`,
+      },
+    },
+  ],
+}
